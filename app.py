82da6ef50f303f6a60ad9a6633c7e6688bcb970b
sameena-bajwa <sbajwa@bu.edu>
2017-10-23 18:21:17 -0400

    update date format

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, url_for
 from flask import jsonify
 import requests
 import json
+import datetime
 
 app = Flask(__name__)
 
@@ -14,8 +15,11 @@ def main():
 @app.route("/results", methods=['POST'])
 def success():
 	date = request.form.get('date')
+	d = datetime.datetime.strptime(date, '%m-%d-%Y')
+	temp = d.strftime('%Y-%m-%d')
+
 	
-	return ("date = %s " % date)	### replace with results from API call
+	return ("date = %s " % temp)	### replace with results from API call
 
 
 if __name__ == "__main__":
